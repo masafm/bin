353e6f9c228055f365e7df153d557da393bd1d8a
masafm <masafumi.kashiwagi@datadoghq.com>
2023-02-16 19:59:50 +0900

    update

--- DeepLで翻訳.py
+++ DeepLで翻訳.py
@@ -9,6 +9,7 @@ import subprocess
 import re
 import os
 import json
+import argparse
 
 def decode(s, encodings=('utf8', 'cp932')):
     for encoding in encodings:
@@ -18,24 +19,35 @@ def decode(s, encodings=('utf8', 'cp932')):
             pass
     return ''
 
+parser = argparse.ArgumentParser(
+            prog=sys.argv[0],
+            usage='Provides texts from standard input', # プログラムの利用方法
+            add_help=True, # -h/–help オプションの追加
+            )
+parser.add_argument('-k', '--key', help='DeepL API key')
+parser.add_argument('-l', '--lang', help='Target language')
+args = parser.parse_args()
+
+if not args.key:
+    with open(os.environ['HOME']+'/.deepl') as file:
+        args.key = file.readline()
+if not args.lang:
+    args.lang = 'EN'
+
 input = sys.stdin.buffer.read()
 try:
-    key=''
-    if len(sys.argv) < 2:
-        with open(os.environ['HOME']+'/.deepl') as file:
-            key = file.readline()
-    else:
-        key = sys.argv[1]
-    lang="EN"
-    if len(sys.argv) >= 3:
-        lang = sys.argv[2]
     text='\n'.join(decode(input).splitlines())
-    print(text+"\n-----")
-    cmd = ["curl", "-X", "POST", "https://api-free.deepl.com/v2/translate", "-H", f"Authorization: DeepL-Auth-Key {key}", "-d", "@-", "-d", f"target_lang={lang}"]
-    p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    output, err = p.communicate(input="text="+text)
-    print(json.loads(output)['translations'][0]['text'])
 except BrokenPipeError:
     devnull = os.open(os.devnull, os.O_WRONLY)
     os.dup2(devnull, sys.stdout.fileno())
     sys.exit(1)
+print(text+"\n-----")
+cmd = ["curl",
+       "-X", "POST",
+       "https://api-free.deepl.com/v2/translate",
+       "-H", f"Authorization: DeepL-Auth-Key {args.key}",
+       "-d", "@-",
+       "-d", f"target_lang={args.lang}"]
+p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+out, err = p.communicate(input="text="+text)
+print(json.loads(out)['translations'][0]['text'])
