dfe10c3e99e05bd6af9734f528dd7005d9a95049
masafm <masafumi.kashiwagi@datadoghq.com>
2023-02-20 08:47:33 +0900

    update

--- 引用返信.py
+++ 引用返信.py
@@ -8,6 +8,7 @@ import sys
 import subprocess
 import re
 import os
+import datetime
 
 def decode(s, encodings=('utf8', 'cp932')):
     for encoding in encodings:
@@ -17,13 +18,14 @@ def decode(s, encodings=('utf8', 'cp932')):
             pass
     return ''
 
+d = datetime.datetime.now()
 input = sys.stdin.buffer.read()
-cmd = ["/Applications/CotEditor.app/Contents/SharedSupport/bin/cot"]
-p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-text = ""
+text=""
+count=0
 try:
     for line in decode(input).splitlines():
         newline = re.sub('\r\n$', '\n', line)
+        count += 1
         if newline.startswith('>'):
             text += f">{newline}\n"
         else:
@@ -33,7 +35,9 @@ except BrokenPipeError:
     os.dup2(devnull, sys.stdout.fileno())
     sys.exit(1)
 
-out, err = p.communicate(input=f"""ご担当者様
+path=f"{os.environ['HOME']}/Documents/drafts/{d.strftime('%Y%m%d-%H%M%S')}.txt"
+with open(path, 'w') as f:
+    f.write(f"""ご担当者様
 
 お世話になっております｡ Datadog 柏木です｡
 
@@ -41,3 +45,7 @@ out, err = p.communicate(input=f"""ご担当者様
 
 以上､よろしくお願いいたします｡
 """)
+
+cmd = ["/Applications/CotEditor.app/Contents/SharedSupport/bin/cot", "-l", str(count+5), path]
+p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+out, err = p.communicate()
